d8f048bc7b957c4868c16ddc48f8fc2a2d42160d
sanchse <sanchse@users.noreply.github.com>
2022-02-28 10:33:39 +0100

    Initial commit

---
+++ src/CsharpBasicSkeleton/Greeter.cs
@@ -0,0 +1,10 @@
+﻿namespace CodelyTv.CsharpBasicSkeleton
+{
+    public static class  Greeter
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
---
+++ test/CsharpBasicSkeleton.Tests/GreeterShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CodelyTv.CsharpBasicSkeleton.Tests
+{
+    public class GreeterShould
+    {
+        [Fact]
+        public void Greet_HelloMessage_Receives()
+        {
+            Assert.Equal("Hello Jhon", Greeter.Greet("Jhon"));
+        }
+    }
+}
